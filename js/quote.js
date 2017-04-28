b323049309b444629a00a845897c1b2a6d8eb039
hanabinoir <crigshn@gmail.com>
2017-04-27 21:28:18 -0700

    basically working now

---
+++ js/quote.js
@@ -0,0 +1,32 @@
+$(document).ready(function() {
+    getQuote();
+
+    $("#getQuote").on('click', function (e) {
+        e.preventDefault();
+        getQuote();
+    });
+
+    var quoteText = $(".quote-text").text().trim()
+    var quoteAuthor = $(".pull-left").text().trim();
+
+    $(".twitter-share-button").attr('data-text', quoteText + " - " + quoteAuthor);
+    $.getScript("http://platform.twitter.com/widgets.js");
+})
+
+function getQuote() {
+    $.ajax({
+        headers: {
+          "X-Mashape-Key": "OivH71yd3tmshl9YKzFH7BTzBVRQp1RaKLajsnafgL2aPsfP9V",
+          Accept: "application/json",
+          "Content-Type": "application/x-www-form-urlencoded"
+        },
+        url: 'https://andruxnet-random-famous-quotes.p.mashape.com/cat=',
+        success: function(response) {
+            var r = JSON.parse(response);
+            currentQuote = r.quote;
+            currentAuthor = r.author;
+            $('.quote-text').html(currentQuote);
+            $('.pull-left').html(currentAuthor);
+        }
+    })
+}
